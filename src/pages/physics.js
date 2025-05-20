9e9e8911ba3231a033b23c0692aa7f4670ed3a52
j_zhang <zzjj714@gmail.com>
2025-05-20 17:55:53 +0200

    fixed light, shadows, plane

--- src/pages/physics.js
+++ src/pages/physics.js
@@ -83,7 +83,7 @@ world.addBody(floorBody)
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(500, 500),
     new THREE.MeshStandardMaterial({
-        color: '#777777',
+        color: 'darkcyan',
         metalness: 0.3,
         roughness: 0.4,
         envMap: environmentMapTexture,
@@ -97,18 +97,20 @@ scene.add(floor)
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.1)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
 directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
+directionalLight.shadow.mapSize = new THREE.Vector2(8192, 8192);
+directionalLight.shadow.camera.far = 800;
+directionalLight.shadow.camera.top = 50
+directionalLight.shadow.camera.bottom = -50
+directionalLight.shadow.camera.left = -50
+directionalLight.shadow.camera.right = 50
+directionalLight.color = new THREE.Color('blanchedalmond')
+
+directionalLight.position.set(40, 20, 20)
 scene.add(directionalLight)
 
 /**
@@ -139,7 +141,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(- 3, 3, 3)
+camera.position.set(-10, 10, 10)
 scene.add(camera)
 
 // Controls
@@ -189,7 +191,7 @@ const createSphere = (radius, position) => {
 }
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const boxMaterial = new THREE.MeshStandardMaterial({ metalness: .3, roughness: .4, flatShading: true, envMap: environmentMapTexture })
+const boxMaterial = new THREE.MeshStandardMaterial({ metalness: .7, roughness: .1, envMap: environmentMapTexture })
 
 const createBox = (length, position, rotation) => {
     // mesh
